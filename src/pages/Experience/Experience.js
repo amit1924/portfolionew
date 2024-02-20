5edb40824d3cf2f49a3dd370a437cab824d6c133
amit1924 <amit192400@gmail.com>
2024-02-20 10:39:41 +0530

    content updated

--- src/pages/Experience/Experience.js
+++ src/pages/Experience/Experience.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import './experience.css';
+import React from "react";
+import "./experience.css";
 
 const Experience = () => {
   return (
-    <div className="experience-container" id='wp'>
+    <div className="experience-container" id="wp">
       <h2 className="experience-heading">Experience</h2>
       <div className="experience-item">
-        <div className="experience-title">Frontend Developer</div>
+        <div className="experience-title">Front End Developer</div>
         <div className="experience-position">Veedcom India Private Limited</div>
-        <div className="experience-duration">May2021 - Present</div>
+        <div className="experience-duration">Aug-2022 Present</div>
       </div>
 
       <div className="experience-item">
         <div className="experience-title">Backend Developer</div>
         <div className="experience-position">Veedcom India Private Limited</div>
-        <div className="experience-duration">July 2022 - Present</div>
+        <div className="experience-duration">Dec-2022 - Present</div>
       </div>
 
       <div className="experience-item">
         <div className="experience-title">Full Stack Developer</div>
         <div className="experience-position">Veedcom India Private Limited</div>
-        <div className="experience-duration">March2023 - present</div>
+        <div className="experience-duration">March-2023 - present</div>
       </div>
     </div>
   );
